0ed8b5dbf561833b37505fd712f5330d8dccb0a6
ninacodin <supawankongsapcharoen@gmail.com>
2025-07-04 16:20:53 +0700

    connected database

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
+<?php
+$host = 'localhost';
+$db   = 'admin_dashboard';
+$user = 'root';
+$pass = 'root';  
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "connectin successfully!";
+    
+} catch (PDOException $e) {
+    echo "Database error: " . $e->getMessage();
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
